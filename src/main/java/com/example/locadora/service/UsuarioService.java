6816643d6ab0515e9e1471e697987ed021399793
tnnoixd <antonio.marcatto@outlook.com>
2025-02-05 13:05:48 -0300

    xd

--- src/main/java/com/example/locadora/service/UsuarioService.java
+++ src/main/java/com/example/locadora/service/UsuarioService.java
@@ -15,6 +15,7 @@ public class UsuarioService {
 
     public Usuario registrarUsuario(String nome, int idade) {
         Usuario usuario = new Usuario(contadorId++, nome, idade);
+        usuarios.add(usuario);
         return usuario;
     }
 
